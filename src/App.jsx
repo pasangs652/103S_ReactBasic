e41f3ff3c333025b7571fd76458e416f1861b161
Pasang Sherpa <pasangs652@gmail.com>
2026-02-09 14:03:59 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+import Home from './Home'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<h2>About</h2>} />
+        <Route path="/contact" element={<h2>contact page</h2>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+
+export default App
